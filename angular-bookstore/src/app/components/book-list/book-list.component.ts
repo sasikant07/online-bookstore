ba206b9724fae3210278a3fed827d60f388fb76f
SASIKANTBISWAL <biswalsashikant@gmail.com>
2020-05-09 22:29:50 +0530

    Added routing navigation to the application as per the category.

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -2,24 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 import { Book } from 'src/app/common/book';
 import { BookService } from 'src/app/services/book.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
+  //templateUrl: './book-list.component.html',
+  templateUrl: './book-grid.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
 
   books: Book[];
+  currenCategoryId: number;
 
-  constructor(private _bookService: BookService) { }
+  constructor(private _bookService: BookService, private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.listBooks();
+    this._activatedRoute.paramMap.subscribe(() => {
+      this.listBooks();
+    })
   }
 
   listBooks() {
-    this._bookService.getBooks().subscribe(
+    
+    const hasCategoryId: boolean = this._activatedRoute.snapshot.paramMap.has('id');
+    
+    if(hasCategoryId){
+      this.currenCategoryId = +this._activatedRoute.snapshot.paramMap.get('id');
+    } else {
+      this.currenCategoryId = 1;
+    }
+
+    this._bookService.getBooks(this.currenCategoryId).subscribe(
       data => this.books = data
     )
   }
