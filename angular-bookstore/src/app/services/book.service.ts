2ac2faca6d5a520f45ffada4cb9167fcd7185a4d
SASIKANTBISWAL <biswalsashikant@gmail.com>
2020-05-10 01:25:56 +0530

    Added feature for dynamically adding book categories from the database.

--- angular-bookstore/src/app/services/book.service.ts
+++ angular-bookstore/src/app/services/book.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Book } from '../common/book';
+import { BookCategory } from '../common/book-category';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ import { Book } from '../common/book';
 export class BookService {
 
   private baseUrl = "http://localhost:8080/api/v1/books";
+  private categoryUrl = "http://localhost:8080/api/v1/book-category";
 
   constructor(private httpClient: HttpClient) { }
 
@@ -20,10 +22,22 @@ export class BookService {
       map(response => response._embedded.books)
     );
   }
+
+  getBookCategories(): Observable<BookCategory[]> {
+    return this.httpClient.get<GetResponseBookCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.bookCategory)
+    );
+  }
 }
 
 interface GetResponseBooks {
   _embedded: {
     books: Book[];
+  }
+}
+
+interface GetResponseBookCategory {
+  _embedded: {
+    bookCategory: BookCategory[];
   }
 }
